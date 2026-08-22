fcdcfc3d4e7ef73f4b8830b74f2f244bd2359507
Christian Cleberg <hello@cleberg.net>
2026-08-22 18:16:00 -0500

    Update cask to 1.1.1 (#48)

--- Casks/octosentry.rb
+++ Casks/octosentry.rb
@@ -5,8 +5,8 @@
 #   brew tap krazywarez/tap && brew install --cask octosentry
 
 cask "octosentry" do
-  version "1.1"
-  sha256 "8f53ce0c4810d6590f8095e983da53a5afc3938ac94333bdb1d9846bb86ec48a"
+  version "1.1.1"
+  sha256 "54abd231f8805af9a75a62ce60f065e492d4447f0387d4505d23ab1ade3dabbc"
 
   url "https://github.com/krazywarez/octosentry/releases/download/#{version}/octosentry-#{version}.dmg"
   name "octosentry"
